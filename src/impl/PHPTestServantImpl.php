5bb68eb65838176b691f914fad2dbd4ced58fabd
suhanyujie <suhanyujie@qq.com>
2019-01-29 19:03:32 +0800

    增加PHPTestServantImpl类的内容

---
+++ src/impl/PHPTestServantImpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: suhanyu
+ * Date: 2019-01-29
+ * Time: 18:59
+ */
+
+namespace Server\impl;
+
+
+class PHPTestServantImpl
+{
+
+}
--- tars/tars.proto.php
+++ tars/tars.proto.php
@@ -5,3 +5,14 @@
  * Date: 2019-01-29
  * Time: 18:06
  */
+return [
+    'appName'         => 'PHPTest',
+    'serverName'      => 'PHPServer',
+    'objName'         => 'obj',
+    'withServant'     => true,//决定是服务端,还是客户端的自动生成
+    'tarsFiles'       => array(
+        './example.tars'
+    ),
+    'dstPath'         => '../src/servant',
+    'namespacePrefix' => 'Server\servant',
+];
