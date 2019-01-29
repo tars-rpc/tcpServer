bda999e71e9f7ffbdb25863113ee88bdaef69920
suhanyujie <suhanyujie@qq.com>
2019-01-29 18:24:56 +0800

    增加一些基础内容

--- src/index.php
+++ src/index.php
@@ -5,3 +5,18 @@
  * Date: 2019-01-29
  * Time: 18:05
  */
+require_once(__DIR__."/vendor/autoload.php");
+
+use \Tars\cmd\Command;
+
+//php tarsCmd.php  conf restart
+$config_path = $argv[1];
+$pos = strpos($config_path,"--config=");
+
+$config_path = substr($config_path,$pos+9);
+
+$cmd = strtolower($argv[2]);
+
+$class = new Command($cmd,$config_path);
+$class->run();
+
--- src/services.php
+++ src/services.php
@@ -5,3 +5,9 @@
  * Date: 2019-01-29
  * Time: 18:06
  */
+
+// 以namespace的方式,在psr4的框架下对代码进行加载
+return [
+    'home-api' => '\Server\servant\PHPTest\PHPServer\obj\TestTafServiceServant',
+    'home-class' => '\Server\impl\PHPServerServantImpl'
+];
